bff73a22df8ed38bc23c2fd983686d7ac3f7cc00
Rahul <rahulchodvadiya@gmail.com>
2026-01-18 18:00:29 +0530

    allow deployed host

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://quiz-management-te23.onrender.com/"
 ]
 
 ROOT_URLCONF = 'core.urls'
